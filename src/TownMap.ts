4645b8336e8578ec390490d7c9c18dcf713b8560
ry <sgmrrc@gmail.com>
2019-08-26 17:50:37 +0900

    TownMapのメソッド実装

--- src/TownMap.ts
+++ src/TownMap.ts
@@ -43,7 +43,7 @@ export class TownMap {
     private statusImage = document.createElement("img");
 
     /** マップ表示用内部バッファ */
-    private mapImage;
+    private mapImage = document.createElement("canvas");
 
     /** 町の人のリスト */
     private peopleList: Array<People>;
@@ -66,9 +66,6 @@ export class TownMap {
 		this.charImage.src = charImage;
 		this.statusImage.src = statusImage;
 
-		// マップ表示用裏画面
-		this.mapImage = document.createElement("canvas");
-
 		// 移動探索アルゴリズム
 		const rs = new RouteSearch(streetImage);
 
@@ -84,22 +81,109 @@ export class TownMap {
 		}
 	}
 	// 通路情報が描かれた縮小マップ画像を返します。
-	getStreetImage() {}
+	public getStreetImage(): HTMLImageElement {
+		return this.streetImage;
+	}
+
 	// マップ表示用の裏画面を返します。この画面には人々が描画されています。
-	getMapImage() {}
-	// マップを描画します。マップ画面は「背景」「人々」「前景」の3つのレイヤーで構成されます。
-	drawMapImage() {}
+	public getMapImage(): HTMLCanvasElement {
+		return this.mapImage;
+	}
+
+	// マップを描画します。マップ画面は「背景」「人々」「前景」の
+	// 3つのレイヤーで構成されます。
+	public drawMapImage(): void {
+		const mapWidth = this.mapImage.width;
+		const mapHeight = this.mapImage.height;
+		const ctx = this.mapImage.getContext('2d');
+		// マップ背景の描画
+		ctx.drawImage(this.mapBgImage, 0, 0, mapWidth, mapHeight, this.screenPos.x, this.screenPos.y,
+			this.screenPos.x + mapWidth, this.screenPos.y + mapHeight)
+
+		// 人々の描画
+		for (let i = 0; i < this.peopleList.length; i++) {
+			const people = this.peopleList[i];
+			const srcX = people.dir * 64 + people.walkPattern * 32;
+			const srcY = people.type * 40;
+			const destX = people.pos.x - this.screenPos.x;
+			const destY = people.pos.y - this.screenPos.y - 8;
+
+			ctx.drawImage(this.charImage, destX, destY,
+				destX + TownMap.WIDTH, destY + TownMap.HEIGHT, srcX, srcY,
+				srcX + TownMap.WIDTH, srcY + TownMap.HEIGHT);
+
+			// ゴール到達ステータスを表示
+			if (people.status == People.GOAL) {
+				ctx.drawImage(this.statusImage, destX, destY - 32,
+					destX + 32, destY,
+					0, 0, 32, 32)
+			}
+
+			// 再検索ステータスを表示
+			if (people.status == People.SEARCH) {
+				ctx.drawImage(this.statusImage, destX, destY - 32,
+					destX + 32, destY,
+					32, 0, 64, 32)
+			}
+		}
+
+		// マップ前景の描画
+		ctx.drawImage(this.mapFgImage, 0, 0, mapWidth, mapHeight, this.screenPos.x, this.screenPos.y,
+			this.screenPos.x + mapWidth, this.screenPos.y + mapHeight);
+	}
+
 	// 時間を進め、町の人々を移動させます。移動後に人々はY座標でソートされます。
 	// また、主人公が中心に来るように、マップのスクロール座標も更新します。
-	update() {}
+	public update(): void {
+		// カウンターを進める
+		this.counter++;
+
+		// 人々の更新
+		for (let i = 0; i < this.peopleList.length; i++) {
+			const people = this.peopleList[i];
+			people.update(this.counter);
+		}
+
+		// y座標で人々を並び替え
+		this.peopleList.sort(function(a, b){
+			if (a.pos.y > b.pos.y) return 1;
+			if (a.pos.y < b.pos.y) return -1;
+			return 0;
+		});
+
+		// 主人公が中心になるようスクリーン座標を計算する
+		this.screenPos.x = this.mainPeople.pos.x - TownMap.SOURCE_SIZE * 5;
+		this.screenPos.y = this.mainPeople.pos.y - TownMap.SOURCE_SIZE * 5;
+
+		if (this.screenPos.x < 0) {
+			this.screenPos.x = 0;
+		} else if (this.screenPos.x > TownMap.MAP_W - TownMap.SCREEN_W) {
+			this.screenPos.x = TownMap.MAP_W - TownMap.SCREEN_W;
+		}
+
+		if (this.screenPos.y < 0) {
+			this.screenPos.y = 0;
+		} else if (this.screenPos.y > TownMap.MAP_H - TownMap.SCREEN_H) {
+			this.screenPos.y = TownMap.MAP_H - TownMap.SCREEN_H;
+		}
+
+	}
 	// 主人公を返します。
-	getMainPeople() {}
+	public getMainPeople(): People {
+		return this.mainPeople;
+	}
 	// 人々のリストを返します。
-	getPeopleList() {}
+	public getPeopleList(): Array<People> {
+		return this.peopleList;
+	}
 	// マップのスクロール座標を返します。
-	getScreenPos() {}
+	public getScreenPos(): Point {
+		return this.screenPos;
+	}
 	// 現在のフレームカウントを返します。
-	getCounter() {}
+	public getCounter(): number {
+		return this.counter;
+	}
 	// テスト用
 	inca(a: number) {return a+1;}
 }
