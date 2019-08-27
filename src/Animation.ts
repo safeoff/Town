cf0d51244c9098e0c2064599a7c2905db4d8e962
ry <sgmrrc@gmail.com>
2019-08-27 14:34:45 +0900

    canvas3枚じゃなくて1枚でいけそうなので減らした

--- src/Animation.ts
+++ src/Animation.ts
@@ -2,9 +2,7 @@ import { TownMap } from "./TownMap";
 
 // 街情報を更新し、再描画する。
 export class Animation {
-	private readonly ctx1: CanvasRenderingContext2D;
-	private readonly ctx2: CanvasRenderingContext2D;
-	private readonly ctx3: CanvasRenderingContext2D;
+	private readonly ctx: CanvasRenderingContext2D;
 	private a = 0;
     /** 全体マップ表示位置オフセット */
     private static STREET_X = 16;
@@ -25,15 +23,13 @@ export class Animation {
     /** 町データ */
     private town: TownMap;
 
+	// 町データを作成し、タイマーイベントを開始する。
 	constructor(
 		private readonly canvas1: HTMLCanvasElement,
-		private readonly canvas2: HTMLCanvasElement,
-		private readonly canvas3: HTMLCanvasElement
 		) {
-		this.ctx1 = this.canvas1.getContext('2d');
-		this.ctx2 = this.canvas2.getContext('2d');
-		this.ctx3 = this.canvas2.getContext('2d');
-		window.requestAnimationFrame(() => this.draw());
+			this.ctx = this.canvas1.getContext('2d');
+			this.town = new TownMap();
+			window.requestAnimationFrame(() => this.draw());
 	}
 
 	// 画面の描画を行う。全体マップに人物の位置と主人公の経路を描画し、
@@ -41,14 +37,22 @@ export class Animation {
 	// 拡大マップの具体的な描画に関しては、TownMapを参照のこと。
 	draw() {
 		// 背景の消去
-		this.ctx1.clearRect(0, 0, this.canvas1.width, this.canvas1.height);
-		this.ctx2.clearRect(0, 0, this.canvas1.width, this.canvas1.height);
-		this.ctx3.clearRect(0, 0, this.canvas1.width, this.canvas1.height);
+		this.ctx.clearRect(0, 0, this.canvas1.width, this.canvas1.height);
 		// 全体マップの描画
 		// 実際のマップの1/8の大きさなので、マップ上の座標を8で割れば
 		// 縮小マップにおける表示位置になる。
-		this.ctx1.drawImage(this.town.getStreetImage(), Animation.STREET_X, Animation.STREET_Y)
+		this.ctx.drawImage(this.town.getStreetImage(), Animation.STREET_X, Animation.STREET_Y);
+		this.ctx.fillStyle = "rgb(211, 211, 211)";
+		this.ctx.fillRect(Animation.STREET_X, Animation.STREET_Y, Animation.STREET_W, Animation.STREET_W);
+
+		const mainPeople = this.town.getMainPeople();
+
 		// 人物位置描画
+		const peopleList = this.town.getPeopleList();
+		for (const p of peopleList) {
+			this.ctx.fillStyle = "rgb(255, 240, 0)";
+			this.ctx.fillRect(Animation.STREET_X + p.pos.x / 8, Animation.STREET_Y + p.pos.y / 8, 4, 4);
+		}
 
 		// 主人公の先頭のノードを取得
 
@@ -68,9 +72,7 @@ export class Animation {
 		// 街の情報表示
 
 		// 拡大マップの描画
-		this.ctx1.fillRect(this.a, 0, 10, 10);
-		this.ctx2.fillRect(this.a, 50, 10, 10);
-		this.ctx3.fillRect(this.a, 100, 10, 10);
+		this.ctx.fillRect(this.a, 0, 10, 10);
 		const townmap = new TownMap();
 		this.a = townmap.inca(this.a);
 		window.requestAnimationFrame(() => this.draw());
